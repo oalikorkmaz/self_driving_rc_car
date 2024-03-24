a29329c19eeb472b24b8ea0346143362fcf9e0c1
Onur Ali Korkmaz <onur.korkmaz@hotmail.com>
2024-03-25 00:08:58 +0300

    added parameter to end video playback

--- lanes1.py
+++ lanes1.py
@@ -110,7 +110,9 @@ while(cap.isOpened()):
          line_image = display_lines(frame, averaged_line)
          combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
          cv2.imshow("result", combo_image)
-         cv2.waitKey(1)
+         if cv2.waitKey(1) & 0xFF == ord('q'):
+             break
          time.sleep(0.1)
      else:
          print("Lines not found!")
+    
