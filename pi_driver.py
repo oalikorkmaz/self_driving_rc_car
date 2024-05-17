41d98c39e465e4726e6384448680744bdc3273bb
Onur Ali Korkmaz <onur.korkmaz@hotmail.com>
2024-05-18 00:32:08 +0300

    arduinoya gaz ve donus verileri gonderildi

--- pi_driver.py
+++ pi_driver.py
@@ -21,43 +21,37 @@ throttle_last_three = []
 #Direksiyonu kalibre etmek için
 steering_factor = 1
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-
-def send_data(steering, throtle):
-    
 
 def main():
-    # hata ayıklama için değer
     sh = False
+    th = 0 
     while True:
         cam = picam2.capture_array()
-
         frame = cv2.resize(cam, (640, 480))
         frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        #maskeleme
         frame_masked = masked_image(frame_hsv, sh)
-
-        #Canny Filtresi
         frame_canny = canny(frame_masked, sh)
-
-        #İlgi alanını belirleme (ROI)
         frame_cropped = region_of_interest(frame_canny, sh)
-
-        #Şeritleri algılama
         lines = detect_lines(frame_cropped)
-    
-        #Şeritlerin sol ve sağ da gruplanması
         averaged_line = average_slope_intercept(frame, lines)
         line_frame = display_lines(frame, averaged_line)
-        # Açının oluşturulması
         steering_input_raw = steering_angle(frame, averaged_line, show=sh)
         print("Steering Input Raw", int(steering_input_raw))
-        ser.write(str(int(steering_input_raw)).encode() + b'\n')
-        
+
+        if len(averaged_line) == 2:
+            th += 3
+        else:
+            th -= 3
+
+        throttle_input = max(0, min(th, 50))  # Gaz değerini 0 ile 50 arasında sınırla
+        steering_input_raw = max(0, min(steering_input_raw, 180))  # Açıyı 0 ile 180 arasında sınırla
+
         combo_frame = cv2.addWeighted(frame, 0.8, line_frame, 1, 1)
         cv2.imshow('Result', combo_frame)
+        ser.write(f"{int(steering_input_raw)},{int(throttle_input)}\n".encode())
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
