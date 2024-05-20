136b93f32f23ed134d1445611b5257413b59ca41
Onur Ali Korkmaz <onur.korkmaz@hotmail.com>
2024-05-20 23:50:31 +0300

    bastan asagi degisim

--- pi_driver.py
+++ pi_driver.py
@@ -17,44 +17,24 @@ picam2.start()
 # Initialize serial communication with Arduino
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-# To hold the last 'n' outputs
-steering_last_five = []
-throttle_last_three = []
-
-# Calibration factor for steering
-steering_factor = 1
-
-# Function to smooth the list of values
-def smooth_values(values, window_size):
-    if len(values) < window_size:
-        return sum(values) / len(values)
-    else:
-        return sum(values[-window_size:]) / window_size
-
 def main():
     sh = False
-    th = 0
+    throttle_input = 1
     while True:
         cam = picam2.capture_array()
         frame = cv2.resize(cam, (640, 480))
         frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
         frame_masked = masked_image(frame_hsv, sh)
-        frame_canny = canny(frame_masked, sh)
-        frame_cropped = region_of_interest(frame_canny, sh)
-        lines = detect_lines(frame_cropped)
+        #frame_canny = canny(frame_masked, sh)
+        frame_roi = region_of_interest(frame_masked, sh)
+        lines = detect_lines(frame_roi)
         averaged_line = average_slope_intercept(frame, lines)
         line_frame = display_lines(frame, averaged_line)
-        steering_input_raw = steering_angle(frame, averaged_line, show=sh)
-        print("Steering Input Raw", int(steering_input_raw))
-
-        throttle_input = max(0, min(th, 50))
-        steering_input_raw = max(0, min(steering_input_raw, 180))
-
+        steering = steering_angle(frame, averaged_line, show=sh)
+        ser.write(f"{int(steering)},{int(throttle_input)}\n".encode())
+        print("Steering", int(steering))
         combo_frame = cv2.addWeighted(frame, 0.8, line_frame, 1, 1)
         cv2.imshow('Result', combo_frame)
-
-        ser.write(f"{int(steering_input_raw)},{int(throttle_input)}\n".encode())
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
