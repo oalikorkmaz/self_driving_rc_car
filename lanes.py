b5708d887cf4e672712e337c9b5a0bddb237efba
Onur Ali Korkmaz <onur.korkmaz@hotmail.com>
2024-06-13 13:20:28 +0300

    steering fonksiyonu değiştirildi

--- lanes.py
+++ lanes.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import time
 import math
 
-def masked_image(image, show=False):  # H  S  V
+def masked_image(image, show):  # H  S  V
     lower_thr = np.array([0, 0, 0])
     upper_thr = np.array([179, 255, 87])
     image_masked = cv2.inRange(image, lower_thr, upper_thr)
@@ -12,7 +12,7 @@ def masked_image(image, show=False):  # H  S  V
         cv2.imshow("Masked Frame", image_masked)
     return image_masked
 
-def canny(image, show=False):
+def canny(image, show):
     if len(image.shape) == 3 and image.shape[2] == 3:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     else:
@@ -27,14 +27,14 @@ def canny(image, show=False):
     return canny
 
 
-def region_of_interest(image, show=False):
+def region_of_interest(image, show):
     height = image.shape[0]
     width = image.shape[1]
     mask = np.zeros_like(image)
     cv2.rectangle(mask, (52, (height // 2) + 27), (590, height - 27), 255, -1)  # -1 -> fill
     roi_image = cv2.bitwise_and(image, mask)
     if show:
-        #cv2.imshow("roi mask", mask)
+        cv2.imshow("roi mask", mask)
         cv2.imshow("roi", roi_image)
     return roi_image
 
@@ -129,21 +129,10 @@ def make_coordinates(image, line):
     intercept = line[1]
     height = image.shape[0]
     width = image.shape[1]
-
-    #
-    #
-    #      left      right
-    #     x1,y1      x1,y1
-    #
-    #
-    #
-    # x2,y2              x2,y2
-
-    # y = mx + c
-    # x = (y - c) / m
-
+    
     y1 = int(height / 2)  # middle
     x1 = int((y1 - intercept) / slop)
+
     if x1 < 0:
         x1 = 0
     if x1 > width:
@@ -158,12 +147,11 @@ def make_coordinates(image, line):
 
     return [[x1, y1, x2, y2]]
 
-def steering_angle(image, lane, show=False):
+def steering_angle(image, lane, show):
     try:
         height = image.shape[0]
         width = image.shape[1]
         center_x = width // 2
-        calibration_offset = -103
 
         if len(lane) == 1:
             x1, y1, x2, y2 = lane[0][0]
@@ -173,8 +161,14 @@ def steering_angle(image, lane, show=False):
         elif len(lane) == 2:
             l1x1, l1y1, l1x2, l1y2 = lane[0][0]
             l2x1, l2y1, l2x2, l2y2 = lane[1][0]
-            average_point = (l1x2 + l2x2) / 2
-            x_deviation = int(average_point) - (center_x + calibration_offset)
+            avg_x1 = int((l1x1 + l2x1) / 2)
+            avg_y1 = int((l1y1 + l2y1) / 2)
+            avg_x2 = int((l1x2 + l2x2) / 2)
+            avg_y2 = int((l1y2 + l2y2) / 2)
+            cv2.line(image, (avg_x1, avg_y1), (avg_x2, avg_y2), (0, 0, 255), 3)
+            if show:
+                cv2.imshow("Mid Line", image)
+            
         else:
             raise ValueError("No Lane detected")
 
@@ -183,18 +177,12 @@ def steering_angle(image, lane, show=False):
         if line_length == 0:
             raise ZeroDivisionError("Height of the image results in line_length being zero")
 
-        angle_to_middle_vertical_rad = math.atan(x_deviation / line_length)
-        angle_to_middle_vertical_deg = int(angle_to_middle_vertical_rad * 180.0 / math.pi)
-        steering = angle_to_middle_vertical_deg + 90
-
-        if show:
-            
-            steering_img = cv2.line(image, (center_x, 0), (center_x, height), (0, 255, 0), 1)
-            
-            red_point_x = center_x + x_deviation + calibration_offset
-            steering_img = cv2.circle(steering_img, (red_point_x, int(height / 2)), radius=3, color=(0, 0, 255), thickness=-1)
-            cv2.imshow("Deviation", steering_img)
-
+        # Açı hesaplama
+        dx = avg_x2 - avg_x1
+        dy = avg_y2 - avg_y1
+        steering = math.degrees(math.atan2(dy, dx))
+        steering = int(steering)
+        print(f"Çizginin X eksenine göre açısı: {steering} derece")
         return steering
 
     except ValueError as e:
@@ -206,11 +194,10 @@ def steering_angle(image, lane, show=False):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
         return 90
-
     
 
 
-def perspective_transform(image, show=False):
+def perspective_transform(image, show):
     top_left = [108, 100]
     bottom_left = [0, 240]
     top_right = [555, 100]
@@ -231,57 +218,56 @@ def perspective_transform(image, show=False):
         cv2.imshow("Perspective", transformed_image)
     
     return transformed_image
-        
-sh = False
+
+sh = True
 video = False
 
-
-# # RESİM BAŞLANGIÇ
-# if not video:
-    # image = cv2.imread("utils/img/test_image.jpg")
-    # image = cv2.resize(image, (640, 480))
-    # image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # image_masked = masked_image(image_hsv, show=sh)
-    # #canny_image = canny(image_masked, show=sh)
-    # #perspective_image = perspective_transform(image_masked, show=sh)
-    # roi_image = region_of_interest(image_masked, show=sh)
-    # lines = detect_lines(roi_image)
-    # averaged_line = average_slope_intercept(image, lines)
-    # line_image = display_lines(image, averaged_line)
-    # steering = steering_angle(image, averaged_line, show=sh)
-    # print("Teker Açısı: ", steering)
+# RESİM BAŞLANGIÇ
+if not video:
+    image = cv2.imread("utils/img/test_image.jpg")
+    image = cv2.resize(image, (640, 480))
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    image_masked = masked_image(image_hsv, show=sh)
+    canny_image = canny(image_masked, show=sh)
+    #perspective_image = perspective_transform(canny_image, show=sh)
+    roi_image = region_of_interest(canny_image, show=sh)
+    lines = detect_lines(roi_image)
+    averaged_line = average_slope_intercept(image, lines)
+    line_image = display_lines(image, averaged_line)
+    steering = steering_angle(image, averaged_line, show=False)
+    print("Teker Açısı: ", steering)
     
-    # combo_image = cv2.addWeighted(image, 0.8, line_image, 1, 1)
+    combo_image = cv2.addWeighted(image, 0.8, line_image, 1, 1)
     
-    # # plt.imshow(combo_image)
-    # # plt.show()
+    # plt.imshow(combo_image)
+    # plt.show()
 
-    # cv2.imshow("combo_image", combo_image)
-    # cv2.waitKey(0)
+    cv2.imshow("combo_image", combo_image)
+    cv2.waitKey(0)
 
-# # VİDEO BAŞLANGIÇ
-# else:
-    # cap = cv2.VideoCapture("utils/video/test_video.mp4")
+# VİDEO BAŞLANGIÇ
+else:
+    cap = cv2.VideoCapture("utils/video/test_video.mp4")
 
-    # while cap.isOpened():
-        # _, frame = cap.read()
-        # frame = cv2.resize(frame, (640, 480))
-        # frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # frame_masked = masked_image(frame_hsv, show=sh)
-        # #canny_frame = canny(frame_masked, show=sh)
-        # #frame_perspective = perspective_transform(frame_masked, show=sh)
-        # frame_roi = region_of_interest(frame_masked, show=sh)
-        # lines = detect_lines(frame_roi)
-        # averaged_line = average_slope_intercept(frame, lines)
-        # line_frame = display_lines(frame, averaged_line)
+    while cap.isOpened():
+        _, frame = cap.read()
+        frame = cv2.resize(frame, (640, 480))
+        frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        frame_masked = masked_image(frame_hsv, show=sh)
+        #canny_frame = canny(frame_masked, show=sh)
+        #frame_perspective = perspective_transform(frame_masked, show=sh)
+        frame_roi = region_of_interest(frame_masked, show=sh)
+        lines = detect_lines(frame_roi)
+        averaged_line = average_slope_intercept(frame, lines)
+        line_frame = display_lines(frame, averaged_line)
     
-        # steering = steering_angle(frame, averaged_line, show=False)
-        # print("Teker Açısı: ", steering)
+        steering = steering_angle(frame, averaged_line, show=False)
+        print("Teker Açısı: ", steering)
 
-        # combo_frame = cv2.addWeighted(frame, 0.8, line_frame, 1, 1)
+        combo_frame = cv2.addWeighted(frame, 0.8, line_frame, 1, 1)
 
-        # cv2.imshow("combo_frame", combo_frame)
+        cv2.imshow("combo_frame", combo_frame)
 
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-            # break
-        # time.sleep(0.1)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        time.sleep(0.1)
--- pi_driver.py
+++ pi_driver.py
@@ -15,32 +15,38 @@ picam2.configure("preview")
 picam2.start()
 
 # Initialize serial communication with Arduino
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1.0)
+time.sleep(3)
+ser.reset_input_buffer()
 def main():
     sh = True
     throttle_input = 1
-    while True:
-        cam = picam2.capture_array()
-        frame = cv2.resize(cam, (640, 480))
-        frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        frame_masked = masked_image(frame_hsv, False)
-        frame_canny = canny(frame_masked, False)
-        frame_roi = region_of_interest(frame_canny, sh)
-        lines = detect_lines(frame_roi)
-        averaged_line = average_slope_intercept(frame, lines)
-        line_frame = display_lines(frame, averaged_line)
-        steering = steering_angle(frame, averaged_line, show=True)
-        ser.write(f"{int(steering)}\n".encode())
-        print("Steering", int(steering))
-        combo_frame = cv2.addWeighted(frame, 0.8, line_frame, 1, 1)
-        cv2.imshow('Result', combo_frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    try:
+        while True:
+            cam = picam2.capture_array()
+            frame = cv2.resize(cam, (640, 480))
+            frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            frame_masked = masked_image(frame_hsv, False)
+            frame_canny = canny(frame_masked, False)
+            frame_roi = region_of_interest(frame_canny, sh)
+            lines = detect_lines(frame_roi)
+            averaged_line = average_slope_intercept(frame, lines)
+            line_frame = display_lines(frame, averaged_line)
+            steering = steering_angle(frame, averaged_line, show=True)
+            ser.write(f"{int(steering)}\n".encode('utf-8'))
+            print("Steering", int(steering))
+            combo_frame = cv2.addWeighted(frame, 0.8, line_frame, 1, 1)
+            cv2.imshow('Result', combo_frame)
 
-    cam.release()
-    cv2.destroyAllWindows()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cam.release()
+        cv2.destroyAllWindows()
+    except KeyboardInterrupt:
+        print("Close Serial Communication.")
+        ser.close()
 
 if __name__ == "__main__":
     main()
